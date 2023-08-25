139d2f63dae17907f830e192cd93d1a7bbc98800
yolobert <143176263+yolobert@users.noreply.github.com>
2023-08-25 11:48:46 +0200

    nomnom

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('YOLOOOOOO')
+
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
+
+ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
